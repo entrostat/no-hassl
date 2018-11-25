eda1fcae9691d36258234715108fc261a5aab569
Kerren <kerren.ortlepp@gmail.com>
2018-11-25 19:50:31 +0200

    Added the command shell

---
+++ src/cli/src/cli.ts
@@ -0,0 +1,7 @@
+export class Cli {
+
+    async run() {
+
+    }
+
+}
---
+++ src/cli/src/commands/command.ts
@@ -0,0 +1,32 @@
+import * as yargs from 'yargs';
+
+interface CommandDescription {
+    command: string;
+    description: string;
+    options: {
+        key: string,
+        details: yargs.Options
+    }[];
+}
+
+export abstract class Command<T> {
+    abstract commandDescription: CommandDescription;
+
+    abstract async run(): Promise<T>;
+
+    register(args: yargs.Argv) {
+        args.command(this.commandDescription.command, this.commandDescription.description, (menu) => {
+            this.commandDescription.options.forEach(option => {
+                menu.option(option.key, option.details);
+            });
+            return menu;
+        });
+    }
+
+    sortAlphabetically(otherCommand: Command): -1|0|1 {
+        if (otherCommand.commandDescription.command === this.commandDescription.command) {
+            return 0;
+        }
+        return otherCommand.commandDescription.command > this.commandDescription.command ? -1 : 1;
+    }
+}
